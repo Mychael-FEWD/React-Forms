4703977b7ce379737da0b752b12db0592cdf64d7
fmagmych <mychaelmagnuson@gmail.com>
2023-07-29 22:19:44 -0500

    Authenitcation works properly, adding dynamic message for username based on condition of 8 character requirement, making error messages more meaningful, making input fields requried, adding styling for UX

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import { useState } from "react";
-import "./App.css";
+
 import Authenticate from "./components/Authenticate";
 import SignUpForm from "./components/SignUpForm";
+import "./App.css";
+
 export default function App() {
+  const [token, setToken] = useState(null);
   return (
-    <>
-    <Authenticate />
-    <SignUpForm   />
-   </>
+    <div className="app">
+      <SignUpForm onSetToken={setToken} className="sign-up-form" />
+      <Authenticate token={token} />
+    </div>
   );
 }
-
